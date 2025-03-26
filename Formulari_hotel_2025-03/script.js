180245b3c9754cc1f8efd2dadb12b4510402cbbc
Asaadlink <asalink@proton.com>
2025-03-26 12:06:38 +0100

    ReservaHabitacion finihed

--- Formulari_hotel_2025-03/script.js
+++ Formulari_hotel_2025-03/script.js
@@ -16,3 +16,59 @@ apellidoHTML.addEventListener('change', () => {
         document.getElementById('errorApellido').textContent = "Hay que introducir un texto de al menos dos caracteres"
     }
 })
+
+// CONTROL DE LAS FECHAS
+const entrada = new Date()
+let salida = new Date(entrada)
+salida.setDate(salida.getDate() + 1)
+// alert(entrada.toISOString())
+
+const entradaString = entrada.toISOString().split('T')[0]
+let salidaString = salida.toISOString().split('T')[0]
+
+const entradaHTML = document.getElementById('entrada')
+entradaHTML.setAttribute('value', entradaString)
+entradaHTML.setAttribute('min', entradaString)
+const salidaHTML = document.getElementById('salida')
+salidaHTML.setAttribute('value', salidaString)
+salidaHTML.setAttribute('min', salidaString)
+
+entradaHTML.addEventListener('change', () => {
+    salida = new Date(entradaHTML.value)
+    salida.setDate(salida.getDate() + 1)
+    salidaString = salida.toISOString().split('T')[0]
+    salidaHTML.setAttribute('value', salidaString)
+    salidaHTML.setAttribute('min', salidaString)
+})
+
+// OBTENER EL OBJETO DIALOG (VENTANA EMERGENTE)
+const ventanaReserva = document.getElementById('ventanaReserva')
+
+// FORMULARIO
+const formReserva = document.forms['formReserva']
+
+formReserva.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    const nombre = formReserva['nombre'].value.trim();
+    const apellido = formReserva['apellido'].value.trim();
+
+   
+    if (nombre === "" || apellido === "") {
+        document.getElementById('errorNombre').textContent = nombre === "" ? "Por favor introduzca un nombre." : "";
+        document.getElementById('errorApellido').textContent = apellido === "" ? "Por favor introduzca un apellido." : "";
+        return;
+    }
+
+    // alert(formReserva['nombre'].value)
+    let resumenReservaDialog = `<p>Reserva realizada a nombre de <span>${formReserva['nombre'].value} ${formReserva['apellido'].value}</span></p>`
+    resumenReservaDialog += `<p>Adultos : <span>${formReserva['adultos'].value}</span> - Niños : <span>${formReserva['ninos'].value}</span></p>`
+    resumenReservaDialog += `<p>Fecha de entrada : <span>${formReserva['entrada'].value}</span></p>`
+    resumenReservaDialog += `<p>Fecha de salida : <span>${formReserva['salida'].value}</span></p>`
+    resumenReservaDialog += `<p>Regimen de estancia : <span>${formReserva['estancia'].value}</span></p>`
+
+    document.getElementById('resumenReserva').innerHTML = resumenReservaDialog
+
+    ventanaReserva.showModal()
+
+})
